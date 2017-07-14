679b36384cd5ca558348a4b482f0f95912cf1a30
luizfer9 <luizfer.sap@gmail.com>
2017-07-14 10:21:11 -0600

    Fix error .gitignore

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,7 +46,7 @@
                                                 <!-- <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus> -->
                                                 @if ($errors->has('email'))
                                                 <span class="help-block">
-                                                    <strong>{{ $errors->first('email') }}</strong>
+                                                    <p>- Vuelve a introducir el correo<!-- <strong>{{ $errors->first('email') }}</strong> -->
                                                 </span>
                                                 @endif
                                             </div>
